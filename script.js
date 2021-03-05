fc64f5defcadaff4c76ad717269906bc33cb2ce9
Max Forsberg <max.forsberg@outlook.com>
2021-03-05 14:51:56 +0100

    needs comments

--- script.js
+++ script.js
@@ -1,25 +1,20 @@
-let todos = [
-    {
-        userid: 1,
-        id: '1', 
-        title: 'Tvätta',
-        completed: false,
-    },
-    {
-        userid: 1,
-        id: '2', 
-        title: 'Diska',
-        completed: true,
-    },
-]
+let todos = []
 
 const todoForm = document.querySelector('#todoForm');
 const todoInput = document.querySelector('#todoInput');
 const todosOutput = document.querySelector('#todos');
 
+const fetchTodos = () => {
+    fetch('https://jsonplaceholder.typicode.com/todos?_page=0&_limit=10')
+    .then(res => res.json())
+    .then(data => {
+        todos = data;
+        listTodos();
+    })
+}
 
-const validateTodo = (text) => {
-    let input = document.querySelector(text);
+
+const validateTodo = (input) => {
 
     if(input.value.trim() === '') {
         input.classList.add('is-invalid');
@@ -34,66 +29,75 @@ const validateTodo = (text) => {
     }
 }
 
-todoForm.addEventListener('submit', e => {
-    e.preventDefault();
-
-    if(e.target.parentNode.innerText === 'ADD'){
-        
-        if(validateTodo('#todoInput') === true) {
-            let newTodo = {
-                userid: 1,
-                id: Date.now().toString(), 
-                title: todoInput.value,
-                completed: false,
-            }
-            todos.push(newTodo);
-            listTodos();
-            todoInput.value = '';
-            
-        }
-        else{
-            return
-        }
-    }
-    
-})
-
 const listTodos = () => {
     todosOutput.innerHTML = '';
-  
-    todos.forEach(todos => {
-        
-
-        if(todos.completed === true){
-            todosOutput.innerHTML+=
-            `
-            <div id="${todos.id}" class="rounded p-2 d-flex justify-content-between align-items-center mb-3 bg-success">
-            <h3>${todos.title}</h3>
-                <div>
-                    <button class="btn btn-primary btn-un-finish">Un-finish</button>
-                    <button class="btn btn-danger">Delete</button>
-                </div>
-            </div>`
-        }
-        else if(todos.completed === false){
-            todosOutput.innerHTML+=
-            `
-            <div id="${todos.id}" class="rounded p-2 d-flex justify-content-between align-items-center mb-3 bg-white">
-                <h3>${todos.title}</h3>
-                <div>
-                    <button class="btn btn-primary btn-finish">Finish</button>
-                    <button class="btn btn-danger">Delete</button>
-                </div>
-            </div>`
-        }
+    todos.forEach(todo => {
+        newTodo(todo);
     })
 }
 
+const newTodo = (todo) => {
+
+    if(todo.completed === true){
+        todosOutput.innerHTML+=
+        `
+        <div id="${todo.id}" class="rounded p-2 d-flex justify-content-between align-items-center mb-3 bg-success">
+            <h3>${todo.title}</h3>
+            <div>
+                <button class="btn btn-primary btn-un-finish">Un-finish</button>
+                <button class="btn btn-danger">Delete</button>
+            </div>
+        </div>`
+    }
+    else if(todo.completed === false){
+        todosOutput.innerHTML+=
+        `
+        <div id="${todo.id}" class="rounded p-2 d-flex justify-content-between align-items-center mb-3 bg-white">
+            <h3>${todo.title}</h3>
+            <div>
+                <button class="btn btn-primary btn-finish">Finish</button>
+                <button class="btn btn-danger">Delete</button>
+            </div>
+        </div>`
+    }
+}
+
+const createTodo = (title) => {
+    fetch('https://jsonplaceholder.typicode.com/todos', {
+        method: 'POST', 
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8'
+        },
+        body: JSON.stringify({
+            title: title,
+            completed: false
+        })
+    })
+    .then(res => res.json())
+    .then(data => {
+        let newTodo = {
+            ...data,
+            id: Date.now()
+        }
+        todos.unshift(newTodo);
+        listTodos();
+    })
+}
+
+todoForm.addEventListener('submit', e => {
+    e.preventDefault();
+
+    if(validateTodo(todoInput)){
+        createTodo(todoInput.value); 
+    }
+       
+    todoForm.reset();
+})
 
 todosOutput.addEventListener('click', e => {
     if(e.target.classList.contains('btn-danger')) {
         if(e.target.parentNode.parentNode.classList.contains('bg-success')){
-            todos = todos.filter(newTodo => newTodo.id !== e.target.parentNode.parentNode.id)
+            todos = todos.filter(newTodo=> newTodo.id != e.target.parentNode.parentNode.id) 
         }
         else{
             alert('can not delete an un-finished todo!')
@@ -101,14 +105,14 @@ todosOutput.addEventListener('click', e => {
     }
     if(e.target.classList.contains('btn-finish')){
         for(i=0; i<todos.length; i++){
-            if(todos[i].id === e.target.parentNode.parentNode.id){
+            if(todos[i].id == e.target.parentNode.parentNode.id){
                 todos[i].completed = true;
             }
         }
     }
     if(e.target.classList.contains('btn-un-finish')){
         for(i=0; i<todos.length; i++){
-            if(todos[i].id === e.target.parentNode.parentNode.id){
+            if(todos[i].id == e.target.parentNode.parentNode.id){
                 todos[i].completed = false;
             }
         }
@@ -116,4 +120,4 @@ todosOutput.addEventListener('click', e => {
     listTodos();
 })
 
-listTodos();
+fetchTodos();
