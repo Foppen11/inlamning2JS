67fdd0188fa69c4bc59933afe57d6035ac15448b
Max Forsberg <max.forsberg@outlook.com>
2021-03-08 12:06:41 +0100

    kommenterad & klar

--- script.js
+++ script.js
@@ -4,41 +4,38 @@ const todoForm = document.querySelector('#todoForm');
 const todoInput = document.querySelector('#todoInput');
 const todosOutput = document.querySelector('#todos');
 
-const fetchTodos = () => {
+const fetchTodos = () => {                                                      // ANROPAR DATABASEN OCH TAR NER 10 OBJEKT
     fetch('https://jsonplaceholder.typicode.com/todos?_page=0&_limit=10')
-    .then(res => res.json())
+    .then(res => res.json())                                                    // KONVERTERAR TEXTEN TILL JSON
     .then(data => {
         todos = data;
         listTodos();
     })
 }
 
-
-const validateTodo = (input) => {
-
-    if(input.value.trim() === '') {
+const validateTodo = (input) => {                                               // VALIDERAR TEXTEN SÅ INPUTEN INTE ÄR TOM
+    if(input.value.trim() === '') {                                             // LÄGGER TILL ELLER TAR BORT KLASSER FÖR ATT SKAPA ETT FELMEDELANDE
         input.classList.add('is-invalid');
         input.classList.remove('is-valid');
         input.focus();
         return false;
-    
-    } else {
+    } 
+    else {
         input.classList.remove('is-invalid');
         input.classList.add('is-valid');
         return true;
     }
 }
 
-const listTodos = () => {
+const listTodos = () => {                                                       // GÖR SÅ OUTPUTEN ÄR TOM OCH KALLAR SEDAN PÅ "newTodo"
     todosOutput.innerHTML = '';
     todos.forEach(todo => {
         newTodo(todo);
     })
 }
 
-const newTodo = (todo) => {
-
-    if(todo.completed === true){
+const newTodo = (todo) => {                                                     // SKAPAR HTML-KOD AV VARJE OBJEKT OCH SKRIVER UT DE PÅ HEMSIDAN
+    if(todo.completed === true){                                                // KOLLAR OM "todon" ÄR FÄRDIG OCH GÖR DEN ISF TILL EN COMPLETED todo
         todosOutput.innerHTML+=
         `
         <div id="${todo.id}" class="rounded p-2 d-flex justify-content-between align-items-center mb-3 bg-success">
@@ -49,7 +46,7 @@ const newTodo = (todo) => {
             </div>
         </div>`
     }
-    else if(todo.completed === false){
+    else if(todo.completed === false){                                          // KOLLAR OM "todon" ÄR O-FÄRDIG OCH GÖR DEN ISF TILL EN O-FÄRDIG todo
         todosOutput.innerHTML+=
         `
         <div id="${todo.id}" class="rounded p-2 d-flex justify-content-between align-items-center mb-3 bg-white">
@@ -62,7 +59,7 @@ const newTodo = (todo) => {
     }
 }
 
-const createTodo = (title) => {
+const createTodo = (title) => {                                                 // SKAPAR EN NY TODO OCH "SKICKAR" DE TILL DATABASEN
     fetch('https://jsonplaceholder.typicode.com/todos', {
         method: 'POST', 
         headers: {
@@ -84,34 +81,33 @@ const createTodo = (title) => {
     })
 }
 
-todoForm.addEventListener('submit', e => {
+todoForm.addEventListener('submit', e => {                                     
     e.preventDefault();
 
-    if(validateTodo(todoInput)){
+    if(validateTodo(todoInput)){                                                // KOLLAR OM VALIDERINGEN ÄR OKEJ OCH SKAPAR SEDAN NYA TODON
         createTodo(todoInput.value); 
-    }
-       
+    } 
     todoForm.reset();
 })
 
 todosOutput.addEventListener('click', e => {
-    if(e.target.classList.contains('btn-danger')) {
-        if(e.target.parentNode.parentNode.classList.contains('bg-success')){
-            todos = todos.filter(newTodo=> newTodo.id != e.target.parentNode.parentNode.id) 
+    if(e.target.classList.contains('btn-danger')) {                                             // KOLLAR EFTER KNAPPTRYCK PÅ "DELETE"
+        if(e.target.parentNode.parentNode.classList.contains('bg-success')){                    // KONTROLLERAR SÅ ATT OBJEKTET ÄR FÄRDIGT (COMPLETED)
+            todos = todos.filter(newTodo=> newTodo.id != e.target.parentNode.parentNode.id)     // TAR ISF BORT OBJEKTET OCH FILTRERAR MED HJÄLP AV ID
         }
-        else{
+        else{                                                                                   // ÄR TODON INTE FÄRDIG KOMMER EN ALERT UPP MED ERRORMEDELANDE
             alert('can not delete an un-finished todo!')
         }
     }
-    if(e.target.classList.contains('btn-finish')){
-        for(i=0; i<todos.length; i++){
+    if(e.target.classList.contains('btn-finish')){                                              // KOLLAR EFTER KNAPPTRYCK PÅ FINISH
+        for(i=0; i<todos.length; i++){                                                          // ÄNDRAR OBJEKTETS COMPLETED STATUS FRÅN FALSE TILL TRUE
             if(todos[i].id == e.target.parentNode.parentNode.id){
                 todos[i].completed = true;
             }
         }
     }
-    if(e.target.classList.contains('btn-un-finish')){
-        for(i=0; i<todos.length; i++){
+    if(e.target.classList.contains('btn-un-finish')){                                           // KOLLAR EFTER KNAPPTRYCK PÅ UN-FINISH
+        for(i=0; i<todos.length; i++){                                                          // ÄNDRAR OBJEKTETS COMPLETED STATUS FRÅN TRUE TILL FALSE
             if(todos[i].id == e.target.parentNode.parentNode.id){
                 todos[i].completed = false;
             }
@@ -119,5 +115,4 @@ todosOutput.addEventListener('click', e => {
     }
     listTodos();
 })
-
 fetchTodos();
